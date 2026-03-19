4a2a674cdc6d8f7338efba01de23d86a084524ab
Ruddy Janpierts Correa Grillo <30013868+janpierts@users.noreply.github.com>
2026-03-19 03:40:02 -0500

    uncomment KeyGeneratiorUtils

--- key_service/src/main/java/com/rj/key_service/utils/KeyGeneratorUtils.java
+++ key_service/src/main/java/com/rj/key_service/utils/KeyGeneratorUtils.java
@@ -7,12 +7,11 @@ public class KeyGeneratorUtils {
 
     public static KeyPair generateRSAKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-        generator.initialize(2048); // Seguridad estándar actual
+        generator.initialize(2048);
         return generator.generateKeyPair();
     }
 
     public static String keyToString(Key key) {
-        // Convertimos a Base64 para guardarlo como String en Redis o Archivo
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
