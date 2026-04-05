842cd9b234b4d7bfc37f17c93f953a1308e4ec99
Ruddy Janpierts Correa Grillo <30013868+janpierts@users.noreply.github.com>
2026-04-04 19:18:37 -0500

    add_function: add endpoint post pass keyword

---
+++ key_service/src/main/java/com/rj/key_service/key_pass/infraestructure/persistence/repository/KeysRedisRepository.java
@@ -0,0 +1,10 @@
+package com.rj.key_service.key_pass.infraestructure.persistence.repository;
+
+import com.rj.key_service.key_pass.domain.model.key_pass_Entity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KeysRedisRepository extends CrudRepository<key_pass_Entity, Integer> {
+
+}
